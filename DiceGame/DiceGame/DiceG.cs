f462c36c3ea1ba663e7d2cf577e30b6b40b3825c
mason <kuangminshen@gmail.com>
2021-07-06 22:23:26 +1000

    Dice game with random number

---
+++ DiceGame/DiceGame/DiceG.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiceGame
+{
+    class DiceG
+    {
+        private int Score;
+        public DiceG()
+        {
+            //Initialize anything that we need to: 
+            Score = 0;
+        }
+
+        public void Start()
+        {
+            //Method that starts the game running.
+
+        }
+        private void PlayRound()
+        {
+            //Method that runs one round of rolling and guessing.
+
+        }
+        private void Win()
+        {
+            //Method that increments the score and lets the player know they won.
+        }
+        private void Lose()
+        {
+            //Method that lets the player know they lost.
+        }
+        private void AskToPlayAgain()
+        {
+            //Method that ask to play again
+        }
+    }
+}
---
+++ DiceGame/DiceGame/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using static System.Console;
+namespace DiceGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Demos to do: 
+            // - Random
+            // - IF conditionals
+            // - Switch statements
+
+            Random myRandGenerator = new Random();
+            int randNumber = myRandGenerator.Next(1, 7);
+            WriteLine($"num {randNumber}");
+
+            if (randNumber <= 3)
+            {
+                WriteLine("you got low");
+            }
+            else
+            {
+                WriteLine("you didn't get it");
+            }
+
+            WriteLine("Press any key to exit...");
+            ReadKey();
+        }
+    }
+}

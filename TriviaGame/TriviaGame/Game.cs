3147bfab0d642d900b7a6e64cba96d5a6ce88fa1
mason <kuangminshen@gmail.com>
2021-07-06 14:50:53 +1000

    basic placeholder for Trivia game

---
+++ TriviaGame/TriviaGame/Game.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TriviaGame
+{
+    class Game
+    {
+        public Game()
+        {
+
+        }
+
+        public void Play()
+        {
+            Console.WriteLine("Welcome to Trivia Quest!");
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TriviaGame/TriviaGame/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TriviaGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game currentGame = new Game();
+            currentGame.Play();
+        }
+    }
+}
